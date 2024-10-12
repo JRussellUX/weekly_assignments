6503d9baa602317e03188cf454513b84537fd725
Jen Russell <jennifer@jrussellux.com>
2024-10-12 16:17:32 -0700

    Inital files

--- README.md
+++
@@ -1,2 +0,0 @@
-# it115
-# Module 2 weekly assignment
--- main.py
+++
@@ -1,5 +0,0 @@
-rows = int(input("Enter number of rows: "))
-
-for row in range(1, rows+1):
-    print("  "*(rows-row), end="")
-    print("$ "*(2*row-1))
